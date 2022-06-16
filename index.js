e2ec8ef17de09433df930b1b5f90905ae456d90f
Anita K <anitak@Kelvins-MacBook-Pro.local>
2022-06-16 10:33:12 +0300

    Display list data

--- index.js
+++ index.js
@@ -19,9 +19,31 @@
 //         })
 //         .catch((error) => console.log(error));
 // };
+const renderPerson = (person) => {
+  let card = document.createElement("li");
+  card.className = "card";
+  card.innerHTML = `
+  <h2>Name: ${person.name}</h2>
+    <div class="content hidden">
+        <p>Mass: ${person.mass}Kgs</p>
+        <p>Hair Color: ${person.hair_color}</p>
+        <p>Skin Color: ${person.skin_color}</p>
+    </div>
+    <button class="btn">More Info</button>`;
+  document.getElementById("container").appendChild(card);
+  document.querySelectorAll(".btn").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      document.querySelectorAll(".content").forEach((item) => {
+        item.classList.toggle("hidden");
+        console.log("clicked");
+      });
+    });
+  });
+};
+
 //2. Async Await
 const fetchPeople = async () => {
-  const response = await fetch("https://swapi.dev/api/people/1/");
+  const response = await fetch("https://swapi.dev/api/people/");
   const data = await response.json();
   console.log(data);
   const results = data.results;
